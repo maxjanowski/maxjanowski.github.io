a0677446895bf6a0354fe52be361f32f1d904d4a
brad2014 <brad2014@users.noreply.github.com>
2022-08-22 22:25:57 +0200

    main.js: refactor getFolderMap to get link ids from fids.json

--- assets/main.js
+++ assets/main.js
@@ -37,69 +37,96 @@ function setupPledgeControl() {
   });
 }
 
-function loadEditionLinks(available, e) {
-  let list = fetch('/assets/editions.json')
+function getFolderMap() {
+  return fetch('/assets/fids.json')
     .then( res => res.json())
-    .then ( res => {
-      const list = res.filter( e => e.id && e.title && !!available === !!e.folderId)
-        .sort((a,b) => {
-          return a['title'].toLowerCase().localeCompare(b['title'].toLowerCase());
-        })
-        .map( e => {
-          return `<div>${e.id}</div><div>
-              <a href="/edition?id=${e.id}" title="Access and download this piece.">
-                ${e.title}
-              </a>
-            </div>`;
+    .then( res => {
+      const folderMap = {}
+      res.forEach( e => {
+        if (e.IsDir) {
+          folderMap[e.Name.slice(0,5)] = e.ID
+        }
+      })
+      return folderMap;
+    })
+    .catch( e => {
+      console.error("Unable to read fids file. " + e.message);
+    })
+}
+
+function loadEditionLinks(available, e) {
+  return getFolderMap()
+    .then( folderMap => {
+      return fetch('/assets/editions.json')
+        .then( res => res.json())
+        .then ( res => {
+          const list = res.filter( e => e.id && e.title && !!available === !!folderMap[e.id])
+            .sort((a,b) => {
+              return a['title'].toLowerCase().localeCompare(b['title'].toLowerCase());
+            })
+            .map( e => {
+              return `<div>${e.id}</div><div>
+                  <a href="/edition?id=${e.id}" title="Access and download this piece.">
+                    ${e.title}
+                  </a>
+                </div>`;
+            });
+          e.innerHTML = '<div>Number</div><div>Title</div>'+list.join('');
         });
-      e.innerHTML = '<div>Number</div><div>Title</div>'+list.join('');
-      console.log(list)
-    });
-  return true;
+    })
+    .catch( e => {
+      console.error("Unable to read editions file. " + e.message)
+    })
 }
 
 function loadEditionPage(e) {
   const doc = e.target;
   const id = new URL(doc.URL).searchParams.get('id')
-  fetch('/assets/editions.json')
-    .then ( res => res.json())
-    .then ( res => {
-      let item = res.find((e) => e['id'] === id);
-      if (!item) {
-        location.href = '/404';
-        return false;
-      }
-      let loadImage = false;
-      loadImage = new Promise( (resolve, reject) => {
-        doc.getElementById('preview-image').onload = resolve;
-        doc.getElementById('preview-image').onerror = resolve;
+
+  return getFolderMap()
+  .then ( folderMap => {
+    if (folderMap[id]) {
+      doc.getElementById('folderUrl').href = `https://drive.google.com/drive/folders/${folderMap[id]}?usp=sharing`;
+      doc.getElementById('folderUrl').innerHTML = 'Download Performance Files';
+    }
+    return fetch('/assets/editions.json')
+      .then ( res => res.json())
+      .then ( res => {
+        let item = res.find((e) => e.id === id);
+        if (!item) {
+          location.href = '/404';
+          return false;
+        }
+        let loadImage = false;
+        loadImage = new Promise( (resolve, reject) => {
+          doc.getElementById('preview-image').onload = resolve;
+          doc.getElementById('preview-image').onerror = resolve;
+        })
+        doc.getElementById('preview-image').src = `/images/preview/${id}-620x800.png`;
+        doc.getElementById('preview-pdf').href = `/download/preview/${id}.pdf`;
+        Object.keys(item).forEach(k => {
+          const v = item[k];
+          if ( k == 'title' ) {
+            doc.title = v + ' | The Max Janowski Society';
+            doc.getElementById('h1').innerHTML = v;
+          }
+          if ( k == 'features' ) {
+            const featureList = v.map( e => `<li>${e}</li>`);
+            doc.getElementById('feature-list').innerHTML =
+              `This downloadable edition includes:<ul>${featureList.join('')}</ul>`;
+          }
+          n = doc.getElementById(k);
+          if (n) {
+            n.innerHTML = v;
+          }
+        })
+        return loadImage;
       })
-      doc.getElementById('preview-image').src = `/images/preview/${id}-620x800.png`;
-      doc.getElementById('preview-pdf').href = `/download/preview/${id}.pdf`;
-      Object.keys(item).forEach(k => {
-        const v = item[k];
-        if ( k == 'title' ) {
-          doc.title = v + ' | The Max Janowski Society';
-          doc.getElementById('h1').innerHTML = v;
-        }
-        if ( k == 'folderId' ) {
-          doc.getElementById('folderUrl').href = `https://drive.google.com/drive/folders/${v}?usp=sharing`;
-          doc.getElementById('folderUrl').innerHTML = 'Download Performance Files';
-        }
-        if ( k == 'features' ) {
-          const featureList = v.map( e => `<li>${e}</li>`);
-          doc.getElementById('feature-list').innerHTML =
-            `This downloadable edition includes:<ul>${featureList.join('')}</ul>`;
-        }
-        n = doc.getElementById(k);
-        if (n) {
-          n.innerHTML = v;
-        }
+      .then( res => {
+        doc.getElementById('edition-wrapper').classList.add('ready');
       })
-      return loadImage;
-    })
-    .then( res => {
-      doc.getElementById('edition-wrapper').classList.add('ready');
-    });
-  return true;
+      .catch( e => {
+        console.error("Unable to read editions file. " + e.message)
+      });
+  })
 }
